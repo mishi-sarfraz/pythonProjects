4b952a8e3e3829d6f885ab453c40c0525220562c
mishi-sarfraz <mahwish405@gmail.com>
2021-09-30 04:51:04 -0700

    Different Python Projects

---
+++ birthday Wisher/birthdaywisher.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import smtplib
+from datetime import datetime
+today=(datetime.now().month,(datetime.now().day))
+import pandas as pd
+# pd.read_csv("birthday.csv")
+data=pd.read_csv("birthday.csv")
+dicbirthday={}
+for index,row in data.iterrows():
+  dicbirthday[(row["month"],row["day"])]=row
+if today in dicbirthday:
+    # print(dicbirthday[today])
+   person=dicbirthday[today]
+   filePath="letter.txt"
+   my_email="icodeguru007@gmail.com"
+   password="new@ccount"
+   with open(filePath) as letter:
+     letter=letter.read()
+     letter=letter.replace("[Name]",person["Name"])
+   with smtplib.SMTP("smtp.gmail.com:587") as connection:
+        connection.starttls()
+        connection.login(my_email,password)
+        connection.sendmail(from_addr=my_email,
+        to_addrs=my_email , 
+        msg=f"Subject: Happy Birthday\n\n{letter}".encode('utf-8'))
