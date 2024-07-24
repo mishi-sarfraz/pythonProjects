31a9696691663212201007f85e20a80a7bffd327
mishi-sarfraz <48648405+mishi-sarfraz@users.noreply.github.com>
2024-07-24 10:22:31 +0500

    looking on branch rules

--- birthday Wisher/birthdaywisher.py
+++ birthday Wisher/birthdaywisher.py
@@ -13,7 +13,7 @@ if today in dicbirthday:
    person=dicbirthday[today]
    filePath="letter.txt"
    my_email="icodeguru007@gmail.com"
-   password="n*****nt"
+   password="*****"
    with open(filePath) as letter:
      letter=letter.read()
      letter=letter.replace("[Name]",person["Name"])
